6f3f403e82bd5cb75acfecb797451ab2120fbb62
Jeff Yankson <jjyankson19@gmail.com>
2026-06-22 12:41:14 +0000

    Learn about the advantages of async js

--- ch01/alt.js
+++ ch01/alt.js
@@ -1,6 +1,24 @@
-const items = ['Item1', 'Item2'];
-const person = {
-  name: 'bob',
-};
+// Asynchronous NODE JS features.
+const http = require('node:http');
+const PORT = process.env.PORT || 3000;
 
-module.exports.cart = items;
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/Home'.toLowerCase()) {
+    res.end(`
+      <p> Home page. </p>`);
+  } else {
+    // BLOCKING CODE.
+    for (let i = 0; i < 50000; i++) {
+      for (let j = 0; j < 50000; j++) {
+        console.log(i, j);
+      }
+    }
+    res.end(`
+      <p> 404 - Page not found </p>
+      `);
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
